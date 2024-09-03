8159627856633cc244b443687550bd22f4f2763a
Ettore Landini <ettore.landini@iit.it>
2024-09-03 15:59:59 +0200

    Added two entry points for docker images that help keep everything up to date [TBT]
    Signed-off-by: Ettore Landini <ettore.landini@iit.it>

---
+++ docker/docker_r1ObjectRetrieval/entrypoints/entrypoint_light.sh
@@ -0,0 +1,3 @@
+cd ${ROBOT_CODE}/r1-object-retrieval/ && \
+    git pull && cd build && cmake .. && make -j11 && \
+    cd ${ROBOT_CODE}/tour-guide-robot/ && \ git pull && cd build && cmake .. && make -j11
