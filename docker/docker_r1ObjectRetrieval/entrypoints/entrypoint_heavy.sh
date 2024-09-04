407551063ad3f9d4f80a2adeb4aaeaca8d6d5268
Ettore Landini <ettore.landini@iit.it>
2024-09-04 16:26:23 +0200

    Custom entrypoint mechanism working as expected
    Signed-off-by: Ettore Landini <ettore.landini@iit.it>

--- docker/docker_r1ObjectRetrieval/entrypoints/entrypoint_heavy.sh
+++ docker/docker_r1ObjectRetrieval/entrypoints/entrypoint_heavy.sh
@@ -7,3 +7,4 @@ cd ${ROBOT_CODE}/ycm && git pull && cd build && cmake .. && make -j11 && \
     cd ${ROBOT_CODE}/cer/ && \ git pull && cd build && cmake .. && make -j11 && \
     cd ${ROBOT_CODE}/tour-guide-robot/ && \ git pull && cd build && cmake .. && make -j11 && \
     cd ${ROBOT_CODE}/r1-object-retrieval/ && git pull && cd build && cmake .. && make -j11
+exec "$@"
