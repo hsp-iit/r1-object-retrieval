d14b07e8178f76e3ff2eb0d9194c09a9b102bf54
Ettore Landini <ettore.landini@iit.it>
2025-07-09 10:57:19 +0200

    Parametrized tour-guide-robot git info

--- docker/docker_r1ObjectRetrieval/entrypoints/entrypoint_light.sh
+++ docker/docker_r1ObjectRetrieval/entrypoints/entrypoint_light.sh
@@ -2,12 +2,16 @@
 ros_distro=$ROS_DISTRO
 r1ObrRemote=$R1_OBR_REMOTE
 r1ObrBranch=$R1_OBR_BRANCH
+tourRemote=$TOUR_REMOTE
+tourBranch=$TOUR_BRANCH
 
 
 . /opt/ros/${ros_distro}/setup.sh
 . ${ROBOT_CODE}/yarp-devices-ros2/ros2_interfaces_ws/install/setup.sh
 #. ${ROBOT_CODE}/tour-guide-robot/app/navigation2/scripts/evaluate_ip.sh
-cd ${ROBOT_CODE}/tour-guide-robot/ && git pull && cd build && cmake .. && make -j11
+cd ${ROBOT_CODE}/tour-guide-robot/ && rm -rf build && mkdir build && \
+    (git fetch ${tourRemote} || (git remote add ${tourRemote} https://github.com/${tourRemote}/tour-guide-robot && git fetch ${tourRemote})) && git checkout ${tourBranch} && git pull && cd build && cmake .. && make -j11
 cd ${ROBOT_CODE}/r1-object-retrieval/ && rm stop_sim.sh && rm -rf build && mkdir build && \
     (git fetch ${r1ObrRemote} || (git remote add ${r1ObrRemote} https://github.com/${r1ObrRemote}/r1-object-retrieval && git fetch ${r1ObrRemote})) && git checkout ${r1ObrBranch} && git pull && cd build && cmake .. && make -j11
+
 exec "$@"
--- docker/docker_r1ObjectRetrieval/entrypoints/env.sh
+++ docker/docker_r1ObjectRetrieval/entrypoints/env.sh
@@ -1,6 +1,8 @@
 export R1_OBR_REMOTE=elandini84
 export R1_OBR_BRANCH=test/sim_lobby
 export ROS_DISTRO=iron
+export TOUR_REMOTE=origin
+export TOUR_BRANCH=feature/openWakeWord/exclude
 export ENTRY_POINT=/home/user1/config/.entrypoint_light.sh
 
 xhost + local:docker
