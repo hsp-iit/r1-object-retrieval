7347e1f3b5f9779ad1842d3b070a17fb40229215
Ettore Landini <ettore.landini@iit.it>
2025-11-10 17:33:43 +0100

    Updated entrypoints
    Signed-off-by: Ettore Landini <ettore.landini@iit.it>

--- docker/docker_r1ObjectRetrieval/entrypoints/env.sh
+++ docker/docker_r1ObjectRetrieval/entrypoints/env.sh
@@ -1,8 +1,8 @@
-export R1_OBR_REMOTE=elandini84
-export R1_OBR_BRANCH=test/sim_lobby
+export R1_OBR_REMOTE=origin
+export R1_OBR_BRANCH=master
 export ROS_DISTRO=iron
 export TOUR_REMOTE=origin
-export TOUR_BRANCH=feature/openWakeWord/exclude
+export TOUR_BRANCH=jazzy
 export ENTRY_POINT=/home/user1/config/.entrypoint_light.sh
 
 xhost + local:docker
--- docker/docker_r1ObjectRetrieval/entrypoints/env_lobby.sh
+++ docker/docker_r1ObjectRetrieval/entrypoints/env_lobby.sh
@@ -1,8 +1,8 @@
-export R1_OBR_REMOTE=elandini84
-export R1_OBR_BRANCH=test/sim_lobby
+export R1_OBR_REMOTE=origin
+export R1_OBR_BRANCH=master
 export ROS_DISTRO=jazzy
 export TOUR_REMOTE=origin
-export TOUR_BRANCH=feature/openWakeWord/exclude
+export TOUR_BRANCH=jazzy
 export ENTRY_POINT=/home/user1/config/.entrypoint_lobby.sh
 
 xhost + local:docker
